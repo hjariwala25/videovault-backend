b76f59306af919192ec5fed97634df63d0f0126b
hjariwala25 <hjariwala150@gmail.com>
2024-12-28 12:49:01 +0530

    Add cookie-parser and cors middleware, implement error handling classes, and enhance server startup logic

--- src/index.js
+++ src/index.js
@@ -5,4 +5,15 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+    // app.on("error", (err) => {
+    //   console.log("Error in server", err);
+    // });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
