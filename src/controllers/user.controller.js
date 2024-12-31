0218af99835d1f674ee4e77800ed58d20749f5cc
hjariwala25 <hjariwala150@gmail.com>
2024-12-31 14:46:24 +0530

    Enhance user registration with validation and file upload support

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,33 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-   res.status(200).json({
-    message: "ok",
+  const { fullname, email, username, password } = req.body;
+  console.log(fullname, email, username, password);
+
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if(existedUser) {
+    throw new ApiError(409, "User with email or username already existed");
+  }
+  
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if(!avatarLocalPath){
+    throw new ApiError(400, "Avatar file is required");
+  }
+  
+
 });
 
-
-export { registerUser }; 
+export { registerUser };
